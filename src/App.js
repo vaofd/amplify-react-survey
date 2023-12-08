00dc0b256be9c35c9873040ddf36aacd49493007
Kishore <kishorev@adpmn.com>
2023-12-08 11:40:40 -0500

    QuickSight dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const iframe = '<iframe src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/589013784134/dashboards/01efec0f-a7e7-4357-b98b-c42986f3167e/sheets/01efec0f-a7e7-4357-b98b-c42986f3167e_63cfefee-7803-4d6d-9e56-cbcc01456335/visuals/01efec0f-a7e7-4357-b98b-c42986f3167e_f3b13e90-c1c2-455e-ba4c-07fd40639414?directory_alias=adpmndev" height="540" width="450" title="SurveyDashBoard"</iframe>';
+
+function Iframe(props) {
+  return (<div dangerouslySetInnerHTML={ {__html:  this.props.iframe? this.props.iframe : ""}} />);
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Survey App Dashboard</h1>
+      <iframe src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/589013784134/dashboards/506ba3e4-c5f3-41e8-9603-dea646d19582/sheets/506ba3e4-c5f3-41e8-9603-dea646d19582_63cfefee-7803-4d6d-9e56-cbcc01456335/visuals/506ba3e4-c5f3-41e8-9603-dea646d19582_68b50ecb-b62f-4648-a338-c72e56214327?directory_alias=adpmndev" height="540" width="450" title="SurveyDashBoard" />
     </div>
   );
 }
