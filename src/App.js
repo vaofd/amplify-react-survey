a45fcb568be6e13ecf655de13f9b664b73c98fc1
Kishore <kishorev@adpmn.com>
2023-12-11 19:13:48 -0500

    Visible window dimensions update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Survey App Dashboard</h1>
-      <iframe src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/589013784134/dashboards/506ba3e4-c5f3-41e8-9603-dea646d19582/sheets/506ba3e4-c5f3-41e8-9603-dea646d19582_63cfefee-7803-4d6d-9e56-cbcc01456335/visuals/506ba3e4-c5f3-41e8-9603-dea646d19582_68b50ecb-b62f-4648-a338-c72e56214327?directory_alias=adpmndev" height="540" width="450" title="SurveyDashBoard" />
+      <iframe src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/589013784134/dashboards/506ba3e4-c5f3-41e8-9603-dea646d19582/sheets/506ba3e4-c5f3-41e8-9603-dea646d19582_63cfefee-7803-4d6d-9e56-cbcc01456335/visuals/506ba3e4-c5f3-41e8-9603-dea646d19582_68b50ecb-b62f-4648-a338-c72e56214327?directory_alias=adpmndev" height="540" width="750" title="SurveyDashBoard" />
     </div>
   );
 }
